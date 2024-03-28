7ccd221227651af695fa5b2872bb10f941b147cd
radhikaganesh29 <radhikaganesh29@gmail.com>
2024-03-28 15:37:09 -0400

    added unit tests for addition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { add } = require("./arithmetica")
 const app = express();
 app.use(cors());
 const port = 3000;
-
 app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
+    res.send('Arithmetic service - last updated TYPE-THE-DATE-AND-TIME-HERE');
 });
 app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n)+Number(req.params.m));
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sum = add(n,m);
+        res.json(sum);
 });
 app.listen(port);
